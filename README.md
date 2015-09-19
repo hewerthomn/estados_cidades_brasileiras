ca5ce4fb5d1d96e5c586ff260bb470a72695698c
Éverton Inocêncio <hewerthomn@gmail.com>
2015-09-19 16:30:17 -0400

    created repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# Estados e Cidades Brasileiras
+
+Tabelas com estados e cidades brasileiras com coordenadas de localização do tipo geométrico.
+
+Inspirado em [https://github.com/felipecsl/brazilian_cities_database](https://github.com/felipecsl/brazilian_cities_database)
---
+++ states.sql
@@ -0,0 +1,36 @@
+
+CREATE TABLE states (
+    id integer NOT NULL,
+    name character varying(255) NOT NULL,
+    short_name character varying(255) NOT NULL,
+    slug character varying(255),
+    country_id integer NOT NULL
+);
+
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (6, 'Paraná', 'PR', 'parana', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (5, 'MInas Gerais', 'MG', 'minas-gerais', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (4, 'Rio de Janeiro', 'RJ', 'rio-de-janeiro', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (3, 'São Paulo', 'SP', 'sao-paulo', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (2, 'Santa Catarina', 'SC', 'santa-catarina', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (1, 'Rio Grande do Sul', 'RS', 'rio-grande-do-sul', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (7, 'Acre', 'AC', 'acre', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (8, 'Alagoas', 'AL', 'alagoas', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (9, 'Amazonas', 'AM', 'amazonas', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (10, 'Amapá', 'AP', 'amapa', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (11, 'Bahia', 'BA', 'bahia', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (12, 'Ceará', 'CE', 'ceara', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (13, 'Distrito Federal', 'DF', 'distrito-federal', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (14, 'Espírito Santo', 'ES', 'espirito-santo', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (15, 'Goiás', 'GO', 'goias', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (16, 'Maranhão', 'MA', 'maranhao', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (17, 'Mato Grosso do Sul', 'MS', 'mato-grosso-do-sul', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (18, 'Mato Grosso', 'MT', 'mato-grosso', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (19, 'Pará', 'PA', 'para', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (20, 'Paraíba', 'PB', 'paraiba', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (21, 'Pernambuco', 'PE', 'pernambuco', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (22, 'Piauí', 'PI', 'piaui', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (23, 'Rio Grande do Norte', 'RN', 'rio-grande-do-norte', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (24, 'Rondônia', 'RO', 'rondonia', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (25, 'Roraima', 'RR', 'roraima', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (26, 'Sergipe', 'SE', 'sergipe', 1);
+INSERT INTO states (id, name, short_name, slug, country_id) VALUES (27, 'Tocantins', 'TO', 'tocantins', 1);
